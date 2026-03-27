91d10c25f9ff663132a5b7d1e51da2c316fd8a77
Saylem Shamburger <shamburgersaylem@gmail.com>
2026-03-26 23:14:56 -0500

    Added logging, seems to run fine

--- higgs01-stats/src/main/java/cs120/Stats.java
+++ higgs01-stats/src/main/java/cs120/Stats.java
@@ -17,6 +17,7 @@ public class Stats implements Statistician {
 	private double[] nums;
 
 	public Stats(double... nums) {
+		logger.info("Initializing Stats Class");
 		this.nums = nums;
 	}
 	
@@ -37,12 +38,19 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findMin() {
-		if (nums.length == 0)
+		logger.info("Called findMin()");
+		
+		if (nums.length == 0) {
+			logger.info("nums.length = 0");
 			return 0;
-
+		}
+		
+		logger.debug("nums.length = {}", nums.length);
+		
 		double minimum = nums[0];
 		for (int i = 1; i < nums.length; i++) {
 			minimum = Math.min(minimum, nums[i]);
+			logger.debug("calculates current minimum as {}", minimum);
 		}
 
 		return minimum;
@@ -55,12 +63,19 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findMax() {
-		if (nums.length == 0)
+		logger.info("Called findMax()");
+		
+		if (nums.length == 0) {
+			logger.info("nums.length = 0");
 			return 0;
-
+		}
+		
+		logger.debug("nums.length = {}", nums.length);
+		
 		double maximum = nums[0];
 		for (int i = 1; i < nums.length; i++) {
 			maximum = Math.max(maximum, nums[i]);
+			logger.debug("calculates current maximum as {}", maximum);
 		}
 
 		return maximum;
@@ -73,13 +88,22 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findMean() {
-		if (nums.length == 0)
+		logger.info("Called findMean()");
+		
+		if (nums.length == 0) {
+			logger.info("nums.length = 0");
 			return 0;
-
+		}
+		
+		logger.debug("nums.length = {}", nums.length);
+		
 		double sum = 0;
 		for (double value : nums) {
 			sum += value;
+			logger.debug("calculates current sum as {}", sum);
 		}
+		
+		logger.debug("current mean is {}", sum / nums.length);
 
 		return sum / nums.length;
 	}
@@ -91,20 +115,30 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findMedian() {
-		if (nums.length == 0)
+		logger.info("Called findMedian()");
+		
+		if (nums.length == 0) {
+			logger.info("nums.length = 0");
 			return 0;
+		}
 
 		double[] localData = nums.clone();
 		Arrays.sort(localData);
+		
+		logger.info("Cloned and sorted localData into an array");
+		
+		logger.debug("nums.length = {}", nums.length);
 
 		if (nums.length % 2 == 0) {
 			// Even number of elements
 			int index1 = localData.length / 2 - 1;
 			int index2 = localData.length / 2;
 			double sum = localData[index1] + localData[index2];
+			logger.debug("calculates current median as {}", sum/2);
 			return sum / 2;
 		} else {
 			// Odd number of elements
+			logger.debug("calculates current median as {}", localData[localData.length / 2]);
 			return localData[localData.length / 2];
 		}
 	}
@@ -117,8 +151,14 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findVariance() {
-		if (nums.length == 0)
+		logger.info("Called findVariance()");
+		
+		if (nums.length == 0) {
+			logger.info("nums.length = 0");
 			return 0;
+		}
+		
+		logger.debug("nums.length = {}", nums.length);
 
 		double mean = findMean();
 		double sumSquaredDiffs = 0;
@@ -126,6 +166,8 @@ public class Stats implements Statistician {
 			double diff = value - mean;
 			sumSquaredDiffs += diff * diff;
 		}
+		
+		logger.debug("calculates current variance as {}", sumSquaredDiffs / nums.length);
 
 		return sumSquaredDiffs / nums.length;
 	}
@@ -137,14 +179,24 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findQ1() {
-		if (nums.length < 2)
+		logger.info("Called findQ1()");
+		
+		logger.debug("nums.length = {}", nums.length);
+		
+		if (nums.length < 2) {
+			logger.info("length is < 2, too short");
 			return 0; // Too few values for a first quartile
+		}
 
 		double[] localNumbers = nums.clone();
 		Arrays.sort(localNumbers);
+		
+		logger.info("Cloned and sorted localData into an array");
 
 		// Integer division properly removes the middle number automatically
 		double[] lowerHalf = Arrays.copyOfRange(localNumbers, 0, localNumbers.length / 2);
+		
+		logger.info("Cut array in half");
 
 		return new Stats(lowerHalf).findMedian();
 	}
@@ -156,11 +208,19 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findQ3() {
-		if (nums.length < 2)
+		logger.info("Called findQ3()");
+		
+		logger.debug("nums.length = {}", nums.length);
+		
+		if (nums.length < 2) {
+			logger.info("length is < 2, too short");
 			return 0; // Too few values for a third quartile
+		}
 
 		double[] localNumbers = nums.clone();
 		Arrays.sort(localNumbers);
+		
+		logger.info("Cloned and sorted localData into an array");
 
 		double[] upperHalf;
 
@@ -173,6 +233,8 @@ public class Stats implements Statistician {
 			upperHalf = Arrays.copyOfRange(localNumbers, localNumbers.length / 2 + 1,
 					localNumbers.length);
 		}
+		
+		logger.info("Cut array in half");
 
 		return new Stats(upperHalf).findMedian();
 	}
@@ -184,6 +246,8 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double findIRQ() {
+		logger.info("Called findIQR()");
+		
 		return this.findQ3() - this.findQ1();
 	}
 
@@ -194,16 +258,25 @@ public class Stats implements Statistician {
 	 */
 	@Override
 	public double[] outliers() {
-		if (nums.length < 2)
+		logger.info("Called outliers()");
+		
+		logger.debug("nums.length = {}", nums.length);
+		
+		if (nums.length < 2) {
+			logger.info("length is < 2, too short");
 			return new double[] {}; // A data set with one value must not have an outlier
-
+		}
+		
 		ArrayList<Double> outlierListBuilder = new ArrayList<Double>();
+		logger.info("Created ArrayList");
+		
 		for (double value : nums) {
 			double iqr = this.findIRQ();
 			double q1 = this.findQ1();
 			double q3 = this.findQ3();
 			if (value < q1 - 1.5 * iqr || value > q3 + 1.5 * iqr) {
 				outlierListBuilder.add(value);
+				logger.debug("Added outlier value of {} to ArrayList", value);
 			}
 		}
 		
@@ -211,11 +284,13 @@ public class Stats implements Statistician {
 		for (int i = 0; i < outlierListBuilder.size(); i++) {
 			outputArray[i] = outlierListBuilder.get(i);
 		}
+		logger.info("Converted ArrayList into an array");
 		return outputArray;
 	}
 
 	@Override
 	public void setNums(double ... numVals) {
+		logger.info("Called setNums()");
 		this.nums = numVals;
 	}
 }
